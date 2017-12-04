3971e7a051bb81a5ca5c4d4f7cf82e3d6ef78ecc
Wezz Balk <wezz@wezz.se>
2017-12-04 15:34:47 +0100

    Added stylelint example

--- configs/webpack.conf.js
+++ configs/webpack.conf.js
@@ -2,13 +2,30 @@
  * @file Webpack dev config for pattern libraries
  * 
  */
+
+const dvmConfig = require('../node_modules/davanmonet/dvm-build/utils/load-config').dvmConfig();
 const LifecyclePlugin = require('../node_modules/davanmonet/dvm-build/plugins/webpack-lifecycle-plugin.js');
 
+// https://github.com/JaKXz/stylelint-webpack-plugin
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
 module.exports = {
-  plugins: [
-    new LifecyclePlugin({"done": (compilation, options, pluginOptions) =>
-    {
-      
-    }}),
-  ]
+	plugins: [
+		new LifecyclePlugin(
+		{
+			"done": (compilation, options, pluginOptions) =>
+			{
+
+			}
+		}),
+		new StyleLintPlugin(
+		{
+			"configFile": "./configs/.stylelintrc",
+			"context":dvmConfig.directories.src,
+			"emitErrors":true,
+			"failOnError":true,
+			"quiet":false,
+			"files":['**/*.s?(a|c)ss', '**/*.less']
+		}),
+	]
 };
