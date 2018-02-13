f3ffb30415efc2c72622d640911dc3d929945b17
Wezz Balk <wezz@wezz.se>
2018-02-13 16:18:18 +0100

    Added examples & configuration for React & Angular components. Better support for Typescript modules

--- configs/webpack.conf.js
+++ configs/webpack.conf.js
@@ -2,7 +2,8 @@
  * @file Webpack dev config for pattern libraries
  * 
  */
-
+const webpack = require('webpack');
+const path = require('path')
 const dvmConfig = require('../node_modules/davanmonet/dvm-build/utils/load-config').dvmConfig();
 const LifecyclePlugin = require('../node_modules/davanmonet/dvm-build/plugins/webpack-lifecycle-plugin.js');
 
@@ -10,7 +11,37 @@ const LifecyclePlugin = require('../node_modules/davanmonet/dvm-build/plugins/we
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
+	module:
+	{
+		rules:
+		[
+			{ /* OPTIONAL: Angular support */
+				test: /\.tsx?$/,
+				loader: [
+					{
+						loader: 'awesome-typescript-loader',
+						options: { configFileName: 'src/tsconfig.json' }
+					},
+					'angular2-template-loader'
+				]
+			},
+			{ /* OPTIONAL: React support */
+				test: /\.jsx?$/,
+				exclude: /.*node_modules((?!davanmonet).)*$/,
+				loader: 'babel-loader',
+				query: {
+					presets:[ 'stage-2', 'react' ]
+				}
+			}
+		]	
+	},
 	plugins: [
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+			require:"requirejs"
+		}),
+		new webpack.ContextReplacementPlugin(/\@angular(\\|\/)core(\\|\/)esm5/, path.join(__dirname, './client')), /* OPTIONAL: Angular support */
 		new LifecyclePlugin(
 		{
 			"done": (compilation, options, pluginOptions) =>
---
+++ src/modules/initiation.ts
@@ -0,0 +1,18 @@
+﻿// /// <reference path="../../node_modules/@types/jquery/index.d.ts" />
+
+import "./managedinitiation/ManagedInitiation";
+import "./ariabuttonmanager/ARIAButtonManager";
+import _MIHelper from "./managedinitiation/ManagedInitiationHelper";
+const appRenderEl =  document.getElementById('showcase__render');
+let showcaseMarkupIsInserted = false;
+if(appRenderEl)
+{
+	window.addEventListener('message', (message) =>
+	{
+		if(typeof message.data === "string" && message.data.indexOf('render-') === 0 && showcaseMarkupIsInserted === false)
+		{
+			_MIHelper.InitiateArea(appRenderEl);
+		}
+	}, false);
+}
+
